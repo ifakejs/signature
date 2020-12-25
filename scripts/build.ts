69671da8ca0adff69747668ef4d257602d198307
BiYuqi <biyuqiwan@163.com>
2020-12-25 23:20:16 +0800

    chore: deal with the d.ts file

---
+++ scripts/build.ts
@@ -0,0 +1,25 @@
+import * as path from 'path'
+import { execSync } from 'child_process'
+import fsExtra from 'fs-extra'
+import chalk from 'chalk'
+
+function resolve(p: string): string {
+  return path.resolve(__dirname, '..', p)
+}
+
+function copyDTS(): void {
+  console.log(chalk.cyan('> start copying .d.ts file to dist dir of packages own.'))
+  const sourceDir = resolve('lib/src')
+  const targetDir = resolve('lib')
+  fsExtra.copySync(sourceDir, targetDir)
+  console.log(chalk.cyan('> copy job is done.'))
+  execSync(`rm -rf ${resolve('lib/src')}`)
+  execSync(`rm -rf ${resolve('dist/src')}`)
+}
+
+try {
+  copyDTS()
+} catch (e) {
+  console.log(e)
+  process.exit(1)
+}
