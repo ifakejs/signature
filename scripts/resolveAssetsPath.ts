c9cec2d28099f60bd5331d00427e3fa589e038b7
BiYuqi <biyuqiwan@163.com>
2020-12-30 10:38:24 +0800

    chore: fix demo issue

--- scripts/resolveAssetsPath.ts
+++ scripts/resolveAssetsPath.ts
@@ -7,9 +7,17 @@ const baseEntry = [
 ]
 
 function task(entrys: string[]): void {
-  entrys.forEach(entry => {
+  entrys.forEach((entry, index) => {
     const data = fs.readFileSync(entry).toString('utf-8')
-    const newData = data.replace(/dist\/index\.umd\.js/, '/signature/index.umd.js')
+    let newData
+    // handle the normal path when come from index.html
+    if (index === 0) {
+      newData = data
+        .replace(/dist\/index\.umd\.js/, '/signature/index.umd.js')
+        .replace(/\/normal\.html/, '/signature/normal.html')
+    } else {
+      newData = data.replace(/dist\/index\.umd\.js/, '/signature/index.umd.js')
+    }
     fs.writeFileSync(entry, newData)
   })
 }
