8724fae109f89a8817370d7b7f34e2697bb929fd
BiYuqi <biyuqiwan@163.com>
2021-01-04 15:10:39 +0800

    chore: release info

--- scripts/release.ts
+++ scripts/release.ts
@@ -17,8 +17,5 @@ if (!allowVersionMark.includes(target)) {
   console.log(chalk.red('不被允许的版本'))
   process.exit(1)
 }
-execSync('yarn run changelog')
-execSync('git add .')
-execSync('git commit -m "docs: update changelog"')
 execSync(`npm version ${target} -m "release: v%s"`)
 console.log(chalk.green('Success to add tag.'))
