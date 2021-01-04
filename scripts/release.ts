2969b7b8a35e9a7766b199f270d937bfe346ad63
BiYuqi <biyuqiwan@163.com>
2021-01-04 10:52:01 +0800

    chore: changelog config

---
+++ scripts/release.ts
@@ -0,0 +1,22 @@
+import { execSync } from 'child_process'
+import chalk from 'chalk'
+
+const target = process.argv[2]
+const allowVersionMark = [
+  'minor',
+  'major',
+  'patch',
+  'premajor',
+  'prepatch',
+  'prerelease',
+  'prerelease',
+  'from-git'
+]
+
+if (!allowVersionMark.includes(target)) {
+  console.log(chalk.red('不被允许的版本'))
+  process.exit(1)
+}
+
+execSync(`npm version ${target} -m "release: v%s"`)
+console.log(chalk.green('Success to add tag.'))
